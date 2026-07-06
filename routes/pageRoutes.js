9e1e25c7200b772d36b783c9c99aa1d6c7eb3425
Lex Briefly <lexbriefly@gmail.com>
2026-07-06 18:24:49 +0530

    Update itle bar

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -5,15 +5,15 @@ const content = require('../controllers/contentController');
 const { requireAuth } = require('../middleware/auth');
 
 router.get('/', content.home);
-router.get('/resource.html', content.resources);
+router.get('/resource', content.resources);
 router.get('/resource/:semester/:subjectCode/:category', content.resourceDetail);
-router.get('/books.html', content.books);
+router.get('/books', content.books);
 router.get('/book/:id', content.bookDetail);
-router.get('/cases.html', content.cases);
+router.get('/cases', content.cases);
 router.get('/case/:id', content.caseDetail);
-router.get('/internship.html', content.internships);
-router.get('/news.html', content.news);
-router.get('/aboutus.html', content.about);
+router.get('/internship', content.internships);
+router.get('/news', content.news);
+router.get('/aboutus', content.about);
 
 router.get('/dashboard', requireAuth, content.studentDashboard);
 
